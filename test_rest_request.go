057d4a090ab1744cc49a14eff51b6e84afa525a5
bill mitsakos <bmitsakos@gmail.com>
2023-12-27 10:19:11 -0700

    Support for custom http handlers such as labstack echo.  Support for custom headers

--- test_rest_request.go
+++ test_rest_request.go
@@ -7,9 +7,24 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
+
 )
 
-func GetEndpoint(path string, responseStruct interface{}, idToken string) error {
+var Handler http.Handler
+
+type Client struct{
+	Headers map[string]string
+}
+
+func init(){
+	if Handler == nil{
+		Handler = http.DefaultServeMux
+	}
+}
+
+func (m Client) GetEndpoint(path string, responseStruct interface{}, idToken string) error {
 
 	req := httptest.NewRequest(http.MethodGet, path, nil)
 
@@ -40,34 +55,50 @@ func GetEndpoint(path string, responseStruct interface{}, idToken string) error 
 	return err
 }
 
-func PostEndpoint(path string, body interface{}, responseStruct interface{}, idToken string) error {
-	return postOrPutEndpoint(http.MethodPost, path, body, responseStruct, idToken)
+func (m Client) PostEndpoint(path string, body interface{}, responseStruct interface{}, idToken string) error {
+	return postOrPutEndpoint(http.MethodPost, m.Headers, path, body, responseStruct, idToken)
 }
-func PutEndpoint(path string, body interface{}, responseStruct interface{}, idToken string) error {
-	return postOrPutEndpoint(http.MethodPut, path, body, responseStruct, idToken)
+func (m Client) PutEndpoint(path string, body interface{}, responseStruct interface{}, idToken string) error {
+	return postOrPutEndpoint(http.MethodPut, m.Headers, path, body, responseStruct, idToken)
 }
-func DeleteEndpoint(path string, body interface{}, responseStruct interface{}, idToken string) error {
-	return postOrPutEndpoint(http.MethodDelete, path, body, responseStruct, idToken)
+func (m Client) DeleteEndpoint(path string, body interface{}, responseStruct interface{}, idToken string) error {
+	return postOrPutEndpoint(http.MethodDelete, m.Headers, path, body, responseStruct, idToken)
 }
-func postOrPutEndpoint(method string, path string, body interface{}, responseStruct interface{}, idToken string) error {
+func postOrPutEndpoint(method string, headers map[string]string, path string, body interface{}, responseStruct interface{}, idToken string) error {
 
-	var buf bytes.Buffer
-	if body != nil {
-		err := json.NewEncoder(&buf).Encode(body)
+	var req *http.Request
+
+	if values, ok := body.(url.Values); ok{
+		var err error
+		req, err = http.NewRequest(method, path, strings.NewReader(values.Encode()))
 		if err != nil {
-			return nil
+			return err
 		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	}else{
+		var buf bytes.Buffer
+		if body != nil {
+			err := json.NewEncoder(&buf).Encode(body)
+			if err != nil {
+				return nil
+			}
+		}
+		reqBody := buf.String()
+		print(reqBody)
+
+		req = httptest.NewRequest(method, path, &buf)
 	}
-	reqBody := buf.String()
 
-	req := httptest.NewRequest(method, path, &buf)
-
+	for key, val := range headers {
+		req.Header.Set(key, val)
+	}
 	if idToken != "" {
 		req.Header.Set("idToken", idToken)
 	}
 
 	w := httptest.NewRecorder()
-	http.DefaultServeMux.ServeHTTP(w, req)
+	Handler.ServeHTTP(w, req)
 	res := w.Result()
 	defer res.Body.Close()
 
@@ -76,7 +107,7 @@ func postOrPutEndpoint(method string, path string, body interface{}, responseStr
 		return err
 	}
 
-	fmt.Print(reqBody)
+	//fmt.Print(reqBody)
 	fmt.Println(res.Status)
 	fmt.Print(string(data))
 
